6ba2da57415a09a8e53b3690cf269b328c1c29b8
Michael Wiederhold <michael.wiederhold@luminari.de>
2022-03-18 09:49:38 +0100

    first commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject clj-tictactoe "0.1.0-SNAPSHOT"
+  :description "A simple Tic Tac Toe implementation"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :main ^:skip-aot clj-tictactoe.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
